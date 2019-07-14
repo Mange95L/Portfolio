0d816201f7a3932c5c1ac1534ef51e2240c1b943
Magnus Landén <magnus.landen@hogia.se>
2019-07-14 18:48:32 +0200

    Feat(index, header) added css for skill badges and badges

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
   /* Your site config here */
 
   siteMetadata: {
-    title: 'Portfolio Site!',
+    title: 'Magnus Landén',
     author: 'Magnus Landén'
   },
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,42 @@ const IndexPage = () => {
 
     return (
         <Layout>
-            <h1>Hello</h1>
-            <h2>I'm Magnus, a full-stack developer living in beautiful Gothenburg.</h2>
-            <p>Want to contact me? <Link to="/contact">Contact me.</Link></p>
+            <h1>Hej!</h1>
+            <h2>Mitt namn är Magnus, Jag är en driven full-stack utvecklare som bor i Göteborg.</h2>
+
+            <h3>Tekniker: </h3>
+
+            <p><strong>Språk: </strong></p>
+            <span className="badge">C#</span>
+            <span className="badge">PHP</span>
+            <span className="badge">JavaScript</span>
+            <span className="badge">SQL</span>
+            <span className="badge">HTML</span>
+            <span className="badge">CSS</span>
+
+            <p><strong>Ramverk: </strong></p>
+            <span className="badge">.NET</span>
+            <span className="badge">.NET CORE</span>
+            <span className="badge">React</span>
+            <span className="badge">Aurelia</span>
+            <span className="badge">Jquery</span>
+            <span className="badge">Sass</span>
+            <span className="badge">Less</span>
+            <span className="badge">Entity Framework</span>
+            <span className="badge">Nhibernate</span>
+
+            <p><strong>Arbetssätt: </strong></p>
+            <span className="badge">SCRUM</span>
+            <span className="badge">TDD</span>
+            <span className="badge">CD</span>
+            <span className="badge">CI</span>
+
+
+            {/* <p><strong>Språk: </strong><span className="badge">C#</span> <span className="badge">PHP</span> <span className="badge">JavaScript</span> <span className="badge"> SQL</span> <span className="badge">HTML</span> <span className="badge"> CSS</span></p>
+            <p><strong>Ramverk: </strong>.NET, .NET CORE, React, Aurelia, Jquery, Sass & Less</p>
+            <p><strong>Arbetssätt: </strong>SCRUM, TDD, continuous development</p> */}
+
+
         </Layout>
     )
 
